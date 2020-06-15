d33806775c1d86105d5a26f1042cb6066fdf5d8c
Windy <laureneuge@Laurens-MBP-17.home>
2020-06-14 23:28:23 -0700

    for loop

--- script.js
+++ script.js
@@ -15,22 +15,19 @@ function writePassword() {
 
     var password = "";
     var passwordArray = Array(length);
-
-    for(var i = 0; i <= complexity; i++){
-        password = passowrd + values.charAt(Math.floor(Math.random() = Math.floor(values.length - 1)));
-      }
-
-    //for (i = 0; i < length; i++) {
-        //var currentOption = options[Math.floor(Math.random() * options.length)];
-        //var randomChar = currentOption.charAt(Math.floor(Math.random() * currentOption.length));
-        //password += randomChar;
-        //passwordArray.push(randomChar);
-}
-  );
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+);
+
+for (i = 0; i < length; i++) {
+    var currentOption = options[Math.floor(Math.random() * options.length)];
+    var randomChar = currentOption.charAt(Math.floor(Math.random() * currentOption.length));
+    password += randomChar;
+    passwordArray.push(randomChar);
 }
+return Array
 
 
 //textbox display
